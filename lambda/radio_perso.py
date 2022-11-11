21f29ea1703872136e44c6a55b31f03519bc30a4
Minifranger <clement.franger@gmail.com>
2022-11-11 16:00:18 +0100

    fix dict slots

--- lambda/radio_perso.py
+++ lambda/radio_perso.py
@@ -35,7 +35,7 @@ class GetMusicIntentHandler(AbstractRequestHandler):
         return ask_utils.is_intent_name("GetMusicIntent")(handler_input)
 
     def handle(self, handler_input):
-        music = handler_input.request_envelope.request.intent.slots.music.value
+        music = handler_input.request_envelope.request.intent.slots.get('music')
         logger.info(f'Got {music} music')
         speak_output = "Hello Python World from Classes!"
 
@@ -138,6 +138,7 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(GetMusicIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
